4a4bacc216dfbd182072c357b62625575b630258
Ramil K <hfvbkmr@mail.ru>
2024-10-20 09:31:54 +0500

    feat: guessing-game class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
+
+    min;
+    max;
+    mid;
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.mid = Math.round((this.max + this.min) / 2)
+        return this.mid;
     }
 
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
-
+        this.min = this.mid;
     }
 }
 
